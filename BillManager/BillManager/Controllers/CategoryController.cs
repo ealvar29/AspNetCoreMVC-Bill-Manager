4f9e7f3f88f45259e5886127776e0c0e77f408c3
Eduardo <edalvarez2015@gmail.com>
2021-01-02 02:21:23 -0600

    Added Razor runtime and Delete view / post

--- BillManager/BillManager/Controllers/CategoryController.cs
+++ BillManager/BillManager/Controllers/CategoryController.cs
@@ -38,9 +38,53 @@ namespace BillManager.Controllers
             {
                 return NotFound();
             }
-
             return View(obj);
         }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+        
+        //GET Delete View
+        public IActionResult Delete(int id)
+        {
+            var obj = _db.Categories.Find(id);
+            
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+        }
+        
+        //POST Delete view
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePost(int? id)
+        {
+            var obj = _db.Categories.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            _db.Categories.Remove(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        
         //GET Create View
         public IActionResult Create()
         {
